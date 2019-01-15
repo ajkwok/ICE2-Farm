6497b096645b036563487492c5b9c622b2489805
ajkwok <35111853+ajkwok@users.noreply.github.com>
2019-01-14 18:59:49 -0800

    Create Horse.java

---
+++ Horse.java
@@ -0,0 +1,43 @@
+public class Horse
+{
+   private String color;
+   private int needsFed;
+   private int needsWashed;
+   private int beenFed;
+   private int beenWashed;
+   private int needsExercised;
+   private int beenExercised;
+   private int beenCombed;
+
+   Horse(String colored, int feed, int wash, int exercise)
+   {
+      color = colored;
+      needsFed = feed;
+      needsWashed = wash;
+      needsExercised = exercise;
+   }
+
+   public void setFed(int fed)
+   {
+      beenFed = fed;
+   }
+   public void setWashed(int washed)
+   {
+      beenWashed = washed;
+   }
+   public void setExercised(int exercised)
+   {
+      beenExercised = exercised;
+   }
+   public void setCombed(int combed)
+   {
+      beenCombed = combed;
+   }
+   public void done()
+   {
+      System.out.println("The horse is " + color + " and has been fed " + beenFed
+                         + " times, washed " + beenWashed + " times, exercised "
+                         + beenExercised + " times, and combed " + beenCombed
+                         + " times.");
+   }
+}
